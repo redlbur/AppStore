f2ab0e0b8411f72d61de31e7fc4f77900f85c804
Mederbek Burkanov <mederburkanov@gmail.com>
2022-10-08 18:30:18 +0600

    refactor ViewControllers

--- AppStoreJSON/AppStoreJSON/BaseTabBarController.swift
+++ AppStoreJSON/AppStoreJSON/BaseTabBarController.swift
@@ -12,54 +12,34 @@ class BaseTabBarController: UITabBarController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    viewControllers = [
+      createNavController(viewController: UIViewController(), title: "Today", imageName: "today_icon.png"),
+      createNavController(viewController: UIViewController(), title: "Apps", imageName: "apps.png"),
+      createNavController(viewController: UIViewController(), title: "Search", imageName: "search.png")
+    ]
+  }
+  
+  fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
     
     let appearance = UINavigationBarAppearance()
     appearance.configureWithOpaqueBackground()
     appearance.backgroundColor = .lightGray
     
+    viewController.navigationItem.title = title
+    viewController.view.backgroundColor = .white
     
-    let todayViewController = UIViewController()
-    todayViewController.view.backgroundColor = .white
-    todayViewController.navigationItem.title = "Today"
+    let navController = UINavigationController(rootViewController: viewController)
+    navController.tabBarItem.title = title
+    navController.tabBarItem.image = UIImage(named: imageName)
+    navController.navigationBar.standardAppearance = appearance
+    navController.navigationBar.scrollEdgeAppearance = navController.navigationBar.standardAppearance
+    navController.navigationBar.prefersLargeTitles = true
     
-    let todayNavController = UINavigationController(rootViewController: todayViewController)
-    todayNavController.tabBarItem.image = UIImage(named: "today_icon.png")
-    todayNavController.tabBarItem.title = "Today"
-    todayNavController.navigationBar.standardAppearance = appearance
-    todayNavController.navigationBar.scrollEdgeAppearance = todayNavController.navigationBar.standardAppearance
-    todayNavController.navigationBar.prefersLargeTitles = true
-    
-
-    let appsViewController = UIViewController()
-    appsViewController.view.backgroundColor = .white
-    appsViewController.navigationItem.title = "Apps"
-    
-    let appsNavController = UINavigationController(rootViewController: appsViewController)
-    appsNavController.tabBarItem.image = UIImage(named: "apps.png")
-    appsNavController.tabBarItem.title = "Apps"
-    appsNavController.navigationBar.standardAppearance = appearance
-    appsNavController.navigationBar.scrollEdgeAppearance = appsNavController.navigationBar.standardAppearance
-    appsNavController.navigationBar.prefersLargeTitles = true
-    
-    
-    let searchViewController = UIViewController()
-    searchViewController.view.backgroundColor = .white
-    searchViewController.navigationItem.title = "Search"
-    
-    let searchNavController = UINavigationController(rootViewController: searchViewController)
-    searchNavController.tabBarItem.image = UIImage(named: "search.png")
-    searchNavController.tabBarItem.title = "Search"
-    searchNavController.navigationBar.standardAppearance = appearance
-    searchNavController.navigationBar.scrollEdgeAppearance = searchNavController.navigationBar.standardAppearance
-    searchNavController.navigationBar.prefersLargeTitles = true
     tabBar.backgroundColor = .lightGray
     tabBar.isTranslucent = true
     
-    viewControllers = [
-      todayNavController,
-      appsNavController,
-      searchNavController
-    ]
+    return navController
+    
   }
   
   
