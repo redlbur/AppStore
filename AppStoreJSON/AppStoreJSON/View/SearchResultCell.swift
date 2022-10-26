56c5dd5d272adf6dd14ac0f30a1d64c5d603efed
Mederbek Burkanov <mederburkanov@gmail.com>
2022-10-26 15:16:41 +0600

    Constrainsts changes

--- AppStoreJSON/AppStoreJSON/View/SearchResultCell.swift
+++ AppStoreJSON/AppStoreJSON/View/SearchResultCell.swift
@@ -12,6 +12,9 @@ class SearchResultCell: UICollectionViewCell {
   let imageView: UIImageView = {
     let iv = UIImageView()
     iv.backgroundColor = .red
+    iv.widthAnchor.constraint(equalToConstant: 64).isActive = true
+    iv.heightAnchor.constraint(equalToConstant: 64).isActive = true
+    iv.layer.cornerRadius = 12
     return iv
   }()
   
@@ -21,32 +24,56 @@ class SearchResultCell: UICollectionViewCell {
     return label
   }()
   
+  let categoryLabel: UILabel = {
+    let label = UILabel()
+    label.text = "Photos & Videos"
+    return label
+  }()
+  
+  let ratingsLabel: UILabel = {
+    let label = UILabel()
+    label.text = "9.26M"
+    return label
+  }()
+  
   let getButton: UIButton = {
     let button = UIButton(type: .system)
     button.setTitle("GET", for: .normal)
     button.setTitleColor(.blue, for: .normal)
     button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+    button.backgroundColor = .darkGray
+    button.widthAnchor.constraint(equalToConstant: 80).isActive = true
+    button.heightAnchor.constraint(equalToConstant: 32).isActive = true
+    button.layer.cornerRadius = 16
     return button
   }()
     
   override init(frame: CGRect) {
     super.init(frame: frame)
     
-    backgroundColor = .green
+    backgroundColor = .yellow
+    
+    let labelsStackView = UIStackView(arrangedSubviews: [
+      nameLabel,
+      categoryLabel,
+      ratingsLabel
+    ])
+    labelsStackView.axis = .vertical
     
     let stackView = UIStackView(arrangedSubviews: [
       imageView,
-      nameLabel,
+      labelsStackView,
       getButton
     ])
     stackView.spacing = 12
+    stackView.alignment = .center
     
     addSubview(stackView)
     stackView.translatesAutoresizingMaskIntoConstraints = false
     stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-    stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+    stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16).isActive = true
     stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-    stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+    stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16).isActive = true
   }
   
   required init?(coder: NSCoder) {
