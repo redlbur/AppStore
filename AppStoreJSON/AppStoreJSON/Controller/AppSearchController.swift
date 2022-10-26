99d2f0b78266346b2e50cba45bfd2ab1f2c28a39
Mederbek Burkanov <mederburkanov@gmail.com>
2022-10-26 20:38:43 +0600

    JSON Fetching Data

--- AppStoreJSON/AppStoreJSON/Controller/AppSearchController.swift
+++ AppStoreJSON/AppStoreJSON/Controller/AppSearchController.swift
@@ -16,8 +16,36 @@ class AppSearchController: UICollectionViewController, UICollectionViewDelegateF
     
     collectionView.backgroundColor = .white
     collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellId)
+    
+    fetchITunesApps()
   }
   
+  
+  fileprivate func fetchITunesApps() {
+    let urlString = "https://itunes.apple.com/search?term=instagram&entity=software"
+    guard let url = URL(string: urlString) else { return }
+    
+    //fetch data from internet
+    URLSession.shared.dataTask(with: url) { data, response, err in
+      if let err = err {
+        print("Failed to fetch apps:", err)
+        return
+      }
+      
+      //success
+//      print(data)
+//      print(String(data: data!, encoding: .utf8))
+      guard let data = data else { return }
+      
+      do {
+        let searchResult = try JSONDecoder().decode(SearchResult.self, from: data)
+        searchResult.results.forEach({print($0.trackName, $0.primaryGenreName)})
+      } catch let jsonError {
+        print("Failed to decode json:", jsonError)
+      }
+      
+    }.resume() //fires off the request
+  }
   
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
     return .init(width: view.frame.width, height: 350)
@@ -28,7 +56,8 @@ class AppSearchController: UICollectionViewController, UICollectionViewDelegateF
     }
   
   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SearchResultCell
+    cell.nameLabel.text = "HERE IS MY APP NAME"
     return cell
   }
   
